c64ba0de368051fe163c99ba153d8796a93723be
Luis GL <lmgl887@gmail.com>
2023-11-17 16:12:59 -0600

    add: branch entity

---
+++ src/main/java/com/ruisu/sectorabackend/entity/Branch.java
@@ -0,0 +1,24 @@
+package com.ruisu.sectorabackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="branches")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String telephone;
+    private String address;
+    private String location;
+}
