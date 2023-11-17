e40cf3708b0dc3ffe4ddd97d6cdb553ed9f1532e
Luis GL <lmgl887@gmail.com>
2023-11-17 16:56:09 -0600

    add: branch repository

---
+++ src/main/java/com/ruisu/sectorabackend/repository/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.ruisu.sectorabackend.repository;
+
+import com.ruisu.sectorabackend.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+}
