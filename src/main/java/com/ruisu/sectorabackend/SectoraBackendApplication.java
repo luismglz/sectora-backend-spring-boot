c774d85e9ab51d4c5cc20f5b224050ccc48a3818
Luis GL <lmgl887@gmail.com>
2023-11-17 14:23:51 -0600

    add: empty project

---
+++ src/main/java/com/ruisu/sectorabackend/SectoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.sectorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectoraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruisu/sectorabackend/SectoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruisu.sectorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
