6314417721d927fa77a5f85173ddb37a0d443f1f
Luis GL <lmgl887@gmail.com>
2023-11-17 20:27:15 -0600

    add: create branch action in controller

--- src/main/java/com/ruisu/sectorabackend/controller/BranchController.java
+++ src/main/java/com/ruisu/sectorabackend/controller/BranchController.java
@@ -3,21 +3,25 @@ package com.ruisu.sectorabackend.controller;
 import com.ruisu.sectorabackend.entity.Branch;
 import com.ruisu.sectorabackend.service.BranchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/")
+@RequestMapping(path = "/api/v1/branches")
 public class BranchController {
 
     @Autowired
     BranchService branchService;
 
-    @GetMapping("/branches")
+    @GetMapping()
     public List<Branch> findAllBranches(){
         return branchService.findAllBranches();
     }
+
+    @PostMapping()
+    public Branch saveBranch(@RequestBody Branch branch){
+        return branchService.saveBranch(branch);
+    }
+
 }
