4abf2a1ce341626a4a971bbeb6d7b6c0d690a262
Luis GL <lmgl887@gmail.com>
2023-11-21 18:51:51 -0600

    add: customer entity annotations

--- src/main/java/com/ruisu/sectorabackend/entity/Customer.java
+++ src/main/java/com/ruisu/sectorabackend/entity/Customer.java
@@ -1,8 +1,8 @@
 package com.ruisu.sectorabackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,11 +10,33 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Table(
+        name = "customer",
+        uniqueConstraints = @UniqueConstraint(
+                name = "email_unique",
+                columnNames = "email"
+        )
+)
 public class Customer {
 
     @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "customer_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
     private Long customerId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
     private String email;
 }
