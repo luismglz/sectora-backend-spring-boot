280e837addaa0a733b412c1ae8effd25c4af383a
Luis GL <lmgl887@gmail.com>
2023-11-24 15:28:19 -0600

    add: manager entity

---
+++ src/main/java/com/ruisu/sectorabackend/entity/Manager.java
@@ -0,0 +1,29 @@
+package com.ruisu.sectorabackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Manager {
+
+    @Id
+    @SequenceGenerator(
+            name = "manager_sequence",
+            sequenceName = "manager_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "manager_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
