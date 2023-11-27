96351f0fa7cca0eaa37add8aa99ec1c174783447
Luis GL <lmgl887@gmail.com>
2023-11-26 22:57:33 -0600

    add: order entity

---
+++ src/main/java/com/ruisu/sectorabackend/entity/Order.java
@@ -0,0 +1,30 @@
+package com.ruisu.sectorabackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tbl_order")
+public class Order {
+
+    @Id
+    @SequenceGenerator(
+            name = "order_sequence",
+            sequenceName = "order_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "order_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long Id;
+    private String description;
+    private double price;
+}
